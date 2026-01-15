e744ab8c392352b82565475b00889d8220a8c9d0
ahmedfarooquipy-debug <ahmedfarooquipy@gmail.com>
2026-01-15 12:45:19 +0530

    Add files via upload

---
+++ ahmedexp5.py
@@ -0,0 +1,23 @@
+'''#Ahmed Farooqui'''
+'''#Uin :- 251P090'''
+print("Ahmed Farooqui")
+print("UIN :- 251P090")
+task_list = [
+    ("Task 1", "2025-02-06","High"),
+    ("Task 2","2025-02-07","Medium"),
+    ("Task 3","2025-02-08","Low"),
+]
+print("Initial Task List:")
+print(task_list)
+new_task = ("Task 4","2025-02-09","High")
+task_list.append(new_task)
+print(task_list)
+task_list.remove(task_list[1])
+print(task_list)
+task_list[1]=("Task 3","2025-02-08","High")
+print(task_list)
+task_list.sort()
+print(task_list)
+
+print("\n Updated Task List:")
+print(task_list)
